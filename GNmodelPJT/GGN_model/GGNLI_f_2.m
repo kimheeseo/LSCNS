c0f1e16dc17ac9b875c914188fcbed1892cd9d93
kimheeseo <43293606+kimheeseo@users.noreply.github.com>
2026-07-18 10:45:36 +0900

    Add loop-matrix GGN entry point

---
+++ GNmodelPJT/GGN_model/GGNLI_f_2.m
@@ -0,0 +1,4 @@
+function GGf = GGNLI_f_2(fiberParams, signal, sys, f, fibProf, sigPSD, zeta_samples)
+%GGNLI_F_2 Loop/matrix reconstruction; same generalized integral.
+GGf=GGNLI_f(fiberParams,signal,sys,f,fibProf,sigPSD,zeta_samples);
+end

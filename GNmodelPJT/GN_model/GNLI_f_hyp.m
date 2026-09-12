a6a0126bd77b27a47d855e3b5f74ed20e0a8531e
kimheeseo <43293606+kimheeseo@users.noreply.github.com>
2026-09-12 21:30:34 +0900

    Delete GNLI_f_hyp.m

--- GNmodelPJT/GN_model/GNLI_f_hyp.m
+++
@@ -1,6 +0,0 @@
-function GNLI = GNLI_f_hyp(fiberParams, signal, sys, f, gtx)
-%GNLI_F_HYP Hyperbolic-coordinate GN-model entry point.
-% Uses the mathematically equivalent Cartesian integral as an executable fallback.
-if nargin<5, gtx=[]; end
-GNLI=GNLI_f(fiberParams,signal,sys,f,gtx);
-end
